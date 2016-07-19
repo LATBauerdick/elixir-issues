7178068da6a3564b4fd5689b102fe316d62d8cdc
LATBauerdick <github@latbauerdick.de>
2016-07-18 22:41:17 -0500

    first version with testing and documentation

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -6,8 +6,12 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
-    parse_args(argv)
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2]
+
+  def main(argv) do
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -18,6 +22,50 @@ defmodule Issues.CLI do
 
   Return a tuple of `{ user, project, count }`, or `:help` if help was given.
   """
+
+  def process( :help ) do
+    IO.puts """
+    usage:  issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt 0
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> convert_to_list_of_maps
+    |> sort(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
+
+  def convert_to_list_of_maps(list) do
+    list
+    |> Enum.map(&Enum.into(&1, Map.new))
+  end
+
+  def sort(list, count) do
+    (if count>0,
+      do: sort_into_ascending_order(list),
+      else: sort_into_descending_order(list)) 
+    |> Enum.take(abs count)
+
+  end
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+      fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
+  end
+  def sort_into_descending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+      fn i1, i2 -> i1["created_at"] >= i2["created_at"] end
+  end
 
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches:  [help: :boolean],
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,7 +2,24 @@ defmodule CliTest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [ parse_args: 1 ]
+  import Issues.CLI, only: [ parse_args: 1,
+                            sort_into_ascending_order: 1,
+                            convert_to_list_of_maps: 1
+                           ]
+
+
+  test "sort ascending orders the correct way" do
+    result =
+      sort_into_ascending_order(fake_created_at_list(~w{c a e b f d}))
+    issues = for issue <- result, do: issue["created_at"]
+    assert issues == ~w{a b c d e f}
+  end
+
+  defp fake_created_at_list(values) do
+    data = for value <- values,
+              do: [{"created_at", value}, {"other_data", "xxx"}]
+    convert_to_list_of_maps data
+  end
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h",     "anything"]) == :help
